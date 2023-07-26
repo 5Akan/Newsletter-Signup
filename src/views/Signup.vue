91a27ac2c7e4d9c969366b8ac0c7a5c32405ebaa
5Akan <akanimohsobot@gmail.com>
2023-07-26 23:53:30 +0100

    Started

---
+++ src/views/Signup.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="signup">
+    <form @submit= "">
+       <label>Email Address</label>
+    <p>
+      <input type="email" name="" id="input" placeholder="email@company.com">
+    </p>
+    </form>
+   
+    
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Signup',
+  setup(){
+    
+  }
+}
+</script>
